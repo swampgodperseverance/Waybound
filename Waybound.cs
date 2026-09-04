163ff1a45f58b60033976968e66dea58a6ad0e91
swampgodperseverance <kirillefsh@gmail.com>
2026-09-04 12:55:02 +0200

    1

--- Waybound.cs
+++ Waybound.cs
@@ -10,6 +10,16 @@ namespace Waybound
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class Waybound : Mod
 	{
+        public static Waybound Instance { get; private set; }
 
-	}
+        public override void Load()
+        {
+            Instance = this;
+        }
+
+        public override void Unload()
+        {
+            Instance = null;
+        }
+    }
 }
